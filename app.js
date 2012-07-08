e94d6df074481f19ffb2716c939b422c78a51bee
xiaobaov2 <xiaobaov2@gmail.com>
2012-07-08 23:49:32 +0800

    modify post data

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ FILE_PATH = __dirname + '/public';
 var http = require('http');
 var bootstrip = require('./lib/bootstrip');
 var config = require('./config');
-var debug = require('./lib/utils/debug').debug;
-
-
-
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res){
-	
+	//req.setEncoding('utf8');
 	bootstrip.run(req, res);
-	
 });
-
 server.listen(config.serverinfo.port);
 
 console.log('Server running... listening port:' + config.serverinfo.port);
