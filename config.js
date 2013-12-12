0991ec96688f30af662a4d73b29286a7e0ec87c3
DK <dukai86@gmail.com>
2013-12-12 17:46:37 +0800

    添加路由配置

--- config.js
+++ config.js
@@ -6,4 +6,43 @@ exports.db = {};
 
 exports.template = {
 	isCache: false
-}
+};
+
+
+exports.routes = [
+	{
+		url: '/v1.1/devices',
+		module: 'api',
+		controller: 'device',
+		action: 'index'
+	},
+
+	{
+		url: '/v1.1/devices/{device_id}',
+		module: 'api',
+		controller: 'device',
+		action: 'show'
+	},
+
+	{
+		url: '/v1.1/devices/{device_id}/sensors',
+		module: 'api',
+		controller: 'device',
+		action: 'sensors'
+	},
+
+	{
+		url: '/v1.1/device/{device_id}/sensor/{sensor_id}.json',
+		module: 'api',
+		controller: 'device',
+		action: 'history-data'
+	},
+
+	{
+		url: '/v1.1/user/api-key',
+		module: 'api',
+		controller: 'device',
+		action: 'history-data'
+	}
+
+];
