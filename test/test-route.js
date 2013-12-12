cf3ccadf54ea84f623d9b33487448e507fb2567d
DK <dukai86@gmail.com>
2013-12-12 21:50:51 +0800

    添加路由测试

---
+++ test/test-route.js
@@ -0,0 +1,19 @@
+var route = require('../lib/route.js');
+
+var r = new route.Route({
+    url: '/v1.1/device/{device_id}/sensor/{sensor_id}.json',
+    module: 'api',
+    controller: 'device',
+    action: 'history-data'
+});
+
+var r2 = new route.Route({
+    url: '/v1.1/devices/{device_id}/sensors',
+    module: 'api',
+    controller: 'device',
+    action: 'sensors'
+});
+
+
+console.log(r.match("/v1.1/device/1221/sensor/2312.json"));
+console.log(r2.match("/v1.1/device/1221/sensor/2312.json"));
