f56014390e379dc7b4c1048c50330c0453c78f75
DK <dukai86@gmail.com>
2013-12-13 15:50:54 +0800

    修改bootstrap名字

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 APP_PATH = __dirname + '/app';
 FILE_PATH = __dirname + '/public';
 var http = require('http');
-var bootstrip = require('./lib/bootstrip');
+var bootstrap = require('./lib/bootstrap');
 var config = require('./config');
-var querystring = require('querystring');
+
+config.app_path = APP_PATH;
+config.file_path = FILE_PATH;
 
 var server = http.createServer(function(req, res){
 	//req.setEncoding('utf8');
-	bootstrip.run(req, res);
+	bootstrap.run(req, res);
 });
 server.listen(config.serverinfo.port);
 
