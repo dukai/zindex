befcfde79027c7796c85402086878f3cee17273d
DK <dukai86@gmail.com>
2013-12-23 18:08:31 +0800

    添加传感器数据帮助类

---
+++ app/helpers/sensor_data_helper.js
@@ -0,0 +1,30 @@
+var SensorData = require('../models/sensor_data');
+
+var SensorDataHelper = {
+
+	validSensorValue: function(data){
+		if(!parseFloat(data.value)){
+			return {
+				status: false,
+				message: SensorData.ERR_MESSAGE.VALUE_FORMAT_INVALID,
+				statusCode: 406
+			};
+		}
+
+
+		if(!!data.timestamp && !Date.parse(data.timestamp)){
+			return {
+				status: false,
+				message: SensorData.ERR_MESSAGE.TIMESTAMP_FORMAT_INVALID,
+				statusCode: 406
+			};
+		}
+
+		return {status: true};
+	}
+
+
+};
+
+
+module.exports = SensorDataHelper;
