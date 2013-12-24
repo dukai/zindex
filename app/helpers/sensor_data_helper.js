977a183ad67b17c4394698229778de6521c6e7b1
DK <dukai86@gmail.com>
2013-12-24 18:00:18 +0800

    添加开关验证

--- app/helpers/sensor_data_helper.js
+++ app/helpers/sensor_data_helper.js
@@ -1,7 +1,11 @@
 var SensorData = require('../models/sensor_data');
 
 var SensorDataHelper = {
-
+	/**
+	 * 验证数据型数据节点有效性
+	 * @param data
+	 * @returns {*}
+	 */
 	validValueSensorData: function(data){
 		if(!parseFloat(data.value)){
 			return {
@@ -16,6 +20,19 @@ var SensorDataHelper = {
 			return {
 				status: false,
 				message: SensorData.ERR_MESSAGE.TIMESTAMP_FORMAT_INVALID,
+				statusCode: 406
+			};
+		}
+
+		return {status: true};
+	},
+
+	validSwitcherSensorData: function(data){
+		var val = parseInt(data.value);
+		if(isNaN(val) || (val !== 1 && val !== 0)){
+			return {
+				status: false,
+				message: SensorData.ERR_MESSAGE.VALUE_FORMAT_INVALID,
 				statusCode: 406
 			};
 		}
