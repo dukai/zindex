ea6ca4aa80cf14fc64474f7091ff0fce3dbc2ea2
DK <dukai86@gmail.com>
2014-01-06 17:00:05 +0800

    添加文件自动上传处理

---
+++ test/redis-files.js
@@ -0,0 +1,37 @@
+var UPYun = require('upyun').UPYun;
+
+var yunClient = new UPYun('foto-sensors', 'dukai', 'shuanger');
+yunClient.setApiDomain('v0.api.upyun.com');
+
+var redis = require('redis'),
+	redisClient = redis.createClient(6379, '192.168.0.35', {
+		detect_buffers: true
+	});
+
+var popAndUploadOne = function(){
+	redisClient.lpop('sensor_photos_queue', function(err, reply){
+		if(!reply){
+			setTimeout(popAndUploadOne, 2000);
+			return;
+		}
+
+		console.log(reply);
+
+		redisClient.get(new Buffer(reply), function(err, fotoReply){
+			var fs = require('fs');
+			var pieces = reply.split(":");
+			var fileName = pieces[pieces.length - 1];
+			fs.writeFile(fileName, fotoReply, function(err){
+				popAndUploadOne();
+			});
+
+
+			yunClient.writeFile('/' + pieces[1] + "/" + pieces[2], fotoReply, true, function(err, result){
+				console.log(err);
+			});
+		})
+	});
+};
+
+
+popAndUploadOne();
